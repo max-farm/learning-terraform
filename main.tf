8945451483d6225baa409f3362d815e3dd8ffd20
max-farm <farmakovskiy@gmail.com>
2023-12-10 14:41:37 +0200

    added pic and VPC config

--- main.tf
+++ main.tf
@@ -12,13 +12,13 @@ provider "aws" {
     }
 }
 
-terraform {
-  backend "s3" {
-    bucket = "tfstate-bucket-20231123133616731900000001" #name of the bucket created my terraform in ./remote-state
-    key = "terraform.tfstate"  #object name of state file 
-    region = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "tfstate-bucket-20231123133616731900000001" #name of the bucket created my terraform in ./remote-state
+#    key = "terraform.tfstate"  #object name of state file 
+#    region = "us-east-1"
+#  }
+#}
 
 ###########data##############
 
@@ -35,7 +35,14 @@ data "aws_ami" "latest_amazon_linux" { #filter names to get latest Amazon Linux 
 
 ###########resource##############
 
-resource "aws_default_vpc" "default" {} # reqiured to get default VPC id
+//resource "aws_default_vpc" "default" {} # required to get default VPC id
+
+resource "aws_vpc" "VPC_1" {
+  cidr_block = "10.10.10.0/25"
+  tags = {
+    Name = "VPC_1"
+  }
+}
 
 resource "aws_default_subnet" "default_az1" {
   availability_zone = data.aws_availability_zones.working.names[0] #return name of 1st AZ in current region
@@ -46,10 +53,82 @@ resource "aws_default_subnet" "default_az2" {
 }
 
 
+resource "aws_subnet" "public_1" {
+  vpc_id = aws_vpc.VPC_1.id
+  cidr_block = "10.10.10.0/27"
+  map_public_ip_on_launch = true
+  tags = { Name = "public_1" }
+  
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id = aws_vpc.VPC_1.id
+  cidr_block = "10.10.10.32/27"
+  map_public_ip_on_launch = true
+  tags = { Name = "public_2" }
+}
+
+resource "aws_eip" "nat_eip" {
+  count = 2
+  domain = "vpc"
+  tags = { Name = "nat-${count.index + 1}-eip" }
+  }
+
+resource "aws_nat_gateway" "nat_gateway_in_subnet_1" {
+  subnet_id     = aws_subnet.public_1.id
+  allocation_id = aws_eip.nat_eip[0].id
+  tags = {
+    Name = "NAT-in-subnet-1"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "nat_gateway_in_subnet_2" {
+  subnet_id     = aws_subnet.public_2.id
+  allocation_id = aws_eip.nat_eip[1].id
+  tags = {
+    Name = "NAT-in-subnet-2"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.VPC_1.id
+  tags = {
+    Name = "IGW"
+  }
+}
+
+resource "aws_route_table" "route_table_public" {
+    vpc_id = aws_vpc.VPC_1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "rt-public" }
+}
+
+resource "aws_route_table_association" "route_table_association_public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.route_table_public.id
+}
+
+resource "aws_route_table_association" "route_table_association_public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.route_table_public.id
+}
+
 
 resource "aws_security_group" "web" {    #defines SG in default VPC
   name   = "Web-Server Security Group"
-  vpc_id = aws_default_vpc.default.id 
+  vpc_id = aws_vpc.VPC_1.id
   dynamic "ingress" {
     for_each = var.allow_ports #define port range from variable
     content {
@@ -93,8 +172,9 @@ resource "aws_autoscaling_group" "web" {
   max_size            = var.env == "prod" ? 2 : 1  # -//-
   min_elb_capacity    = var.env == "prod" ? 2 : 1  # -//-
   health_check_type   = "ELB"
-  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  vpc_zone_identifier = [aws_subnet.public_1.id, aws_subnet.public_2.id]
   target_group_arns = [aws_lb_target_group.web.arn]
+  depends_on = [aws_internet_gateway.igw, aws_nat_gateway.nat_gateway_in_subnet_1, aws_nat_gateway.nat_gateway_in_subnet_2]
   
   launch_template {
     id = aws_launch_template.web.id
@@ -121,7 +201,7 @@ resource "aws_lb" "web" {
   name               = "Web-Server-HighlyAvailable-ALB"
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web.id]
-  subnets            = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
   
   tags =  {
     Name = "Web-Server-HighlyAvailable-ALB" #extra tag
@@ -130,7 +210,7 @@ resource "aws_lb" "web" {
 
 resource "aws_lb_target_group" "web" {
   name                 = "Web-Server-TG"
-  vpc_id               = aws_default_vpc.default.id
+  vpc_id               = aws_vpc.VPC_1.id
   port                 = 80
   protocol             = "HTTP"
   deregistration_delay = 10 #The amount of time for Elastic Load Balancing to wait before deregistering a target. The range is 0–3600 seconds. The default value is 300 seconds.
--- readme.md
+++ readme.md
@@ -1,16 +1,18 @@
 ## Exploring some terraform basics with aws
 
-Building AWS ASG and ALB with web-server on 80 port with instances based on type of environment.
+![image](AWS_ASG.drawio.svg)
 
-Web server installation script provided in web-server.sh 
+  Building AWS ASG and ALB with web-server on 80 port with instances based on type of environment.
 
-Variables in terraform.tvfars can be changed to provide region, enable and disable monitoring and changes Environment - env. 
+- Web server installation script provided in web-server.sh 
 
-If env is "prod" the appropriate tags apllied and ASG scales to 2 instances and instance type t2.large. 
+- Variables in terraform.tvfars can be changed to provide region, enable and disable monitoring and changes Environment - env. 
 
-If env is other than "prod", for example "test", than t3.micro used for 1 instance. 
+- If env is "prod" the appropriate tags apllied and ASG scales to 2 instances and instance type t2.large. 
+
+- If env is other than "prod", for example "test", than t3.micro used for 1 instance. 
  
-outputs.tf provide variables definitions output including load balancer URL.
+- outputs.tf provide variables definitions output including load balancer URL.
 
-remote-state folder contains TF file to create S3 bucket for storing tfstate file
-main.tf contains link to store tfstate file in S3 bucket
+- remote-state folder contains TF file to create S3 bucket for storing tfstate file
+- main.tf contains link to store tfstate file in S3 bucket
